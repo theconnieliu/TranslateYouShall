b4e0b18708c4294c588d9c300db77f2f7f25b108
Connie Liu <connieliu73@gmail.com>
2018-02-18 16:40:18 +0000

    https doesn't work

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#button").click(function () {
         console.log("Working on it")
         $.ajax({
-            url: 'https://35.161.105.159/translate',
+            url: 'http://35.161.105.159/translate',
             type: 'POST',
             data: JSON.stringify({"text":$("#text").val()}),
             contentType: 'application/json; charset=utf-8',
